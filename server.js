b68f4ed82b6bf711dc16bc131774ccaae896942a
Jayden Lee <jaydenlee@Jaydens-MacBook-Pro.local>
2025-04-07 19:17:29 -0700

    sent

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const server = http.createServer();
 function respondToRequestFromBrowser(request, response) {
     const fs = require('fs')
     if (request.url === "/"){
-        response.writeHead(200,{ 'Content-Tpye': 'text.html'}); 
+        response.writeHead(200,{ 'Content-Type': 'text.html'}); 
         const html = fs.readFileSync('eeg.csv', 'utf-8'); 
         response.end(html); 
     } 
